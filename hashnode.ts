019710c874434d1a71112c49af0f2d875a9cfdc8
Ammar Mirza <itsammarmirza@gmail.com>
2024-11-07 16:53:11 +0530

    Update Hashnode.ts

--- hashnode.ts
+++ hashnode.ts
@@ -1,6 +1,6 @@
 // This is ignored for the publish from GH - it is needed for the CLI and local previews
 // THe project ID is connected to the installation ID of the repo, thus we now which project to update
 export default {
-  linkedProjectId: "66826066f496f6d9063fec22",
-  previewProjectId: "66826066f496f6d9063fec22",
+  linkedProjectId: "66d94e2aa51212c5c2894bf9",
+  previewProjectId: "66d94e2aa51212c5c2894bf9",
 };
