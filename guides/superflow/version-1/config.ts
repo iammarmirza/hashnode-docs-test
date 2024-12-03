56984ff9b5d0ae26b522dfba01c6baa62ebab5ee
Ammar Mirza <itsammarmirza@gmail.com>
2024-12-03 11:01:01 +0530

    Add new guide

---
+++ guides/superflow/version-1/config.ts
@@ -0,0 +1,463 @@
+export default {
+    settings: {
+        name: "V1",
+        slug: "v1",
+        isDefault: true,
+    },
+    sidebar: [
+        {
+            type: "section",
+            label: "No Code Platforms",
+            visibility: "PUBLIC",
+            pages: [
+                {
+                    type: "page",
+                    path: "./no-code-platforms/webflow.mdx",
+                    pages: [
+                        {
+                            type: "page",
+                            path: "./no-code-platforms/webflow/custom-code.mdx",
+                            pages: [
+                                {
+                                    type: "page",
+                                    path: "./no-code-platforms/webflow/custom-code/setup.mdx",
+                                },
+                                {
+                                    type: "page",
+                                    path: "./no-code-platforms/webflow/custom-code/video.mdx",
+                                }
+                            ]
+                        },
+                        {
+                            type: "page",
+                            path: "./no-code-platforms/webflow/integration.mdx",
+                            pages: [
+                                {
+                                    type: "page",
+                                    path: "./no-code-platforms/webflow/integration/setup.mdx",
+                                },
+                                {
+                                    type: "page",
+                                    path: "./no-code-platforms/webflow/integration/video.mdx",
+                                }
+                            ]
+                        },
+                    ]
+                },
+                {
+                    type: "page",
+                    path: "./no-code-platforms/bubble.mdx",
+                    pages: [
+                        {
+                            type: "page",
+                            path: "./no-code-platforms/bubble/setup.mdx",
+                        },
+                        {
+                            type: "page",
+                            path: "./no-code-platforms/bubble/video.mdx",
+                        },
+                    ]
+                },
+                {
+                    type: "page",
+                    path: "./no-code-platforms/hubspot.mdx",
+                    pages: [
+                        {
+                            type: "page",
+                            path: "./no-code-platforms/hubspot/setup.mdx",
+                        },
+                        {
+                            type: "page",
+                            path: "./no-code-platforms/hubspot/video.mdx",
+                        },
+                    ]
+                },
+                {
+                    type: "page",
+                    path: "./no-code-platforms/drupal.mdx",
+                    pages: [
+                        {
+                            type: "page",
+                            path: "./no-code-platforms/drupal/setup.mdx",
+                        },
+                        {
+                            type: "page",
+                            path: "./no-code-platforms/drupal/video.mdx",
+                        },
+                    ]
+                },
+                {
+                    type: "page",
+                    path: "./no-code-platforms/wordpress.mdx",
+                    pages: [
+                        {
+                            type: "page",
+                            path: "./no-code-platforms/wordpress/plugin.mdx",
+                            pages: [
+                                {
+                                    type: "page",
+                                    path: "./no-code-platforms/wordpress/plugin/setup.mdx",
+                                },
+                                {
+                                    type: "page",
+                                    path: "./no-code-platforms/wordpress/plugin/video.mdx",
+                                }
+                            ]
+                        },
+                        {
+                            type: "page",
+                            path: "./no-code-platforms/wordpress/self-hosted.mdx",
+                            pages: [
+                                {
+                                    type: "page",
+                                    path: "./no-code-platforms/wordpress/self-hosted/setup.mdx",
+                                },
+                                {
+                                    type: "page",
+                                    path: "./no-code-platforms/wordpress/self-hosted/video.mdx",
+                                }
+                            ]
+                        },
+                        {
+                            type: "page",
+                            path: "./no-code-platforms/wordpress/elementor.mdx",
+                            pages: [
+                                {
+                                    type: "page",
+                                    path: "./no-code-platforms/wordpress/elementor/setup.mdx",
+                                },
+                                {
+                                    type: "page",
+                                    path: "./no-code-platforms/wordpress/elementor/video.mdx",
+                                }
+                            ]
+                        },
+                    ]
+                },
+                {
+                    type: "page",
+                    path: "./no-code-platforms/wix.mdx",
+                    pages: [
+                        {
+                            type: "page",
+                            path: "./no-code-platforms/wix/setup.mdx",
+                        },
+                        {
+                            type: "page",
+                            path: "./no-code-platforms/wix/video.mdx",
+                        },
+                    ]
+                },
+                {
+                    type: "page",
+                    path: "./no-code-platforms/squarespace.mdx",
+                    pages: [
+                        {
+                            type: "page",
+                            path: "./no-code-platforms/squarespace/setup.mdx",
+                        },
+                        {
+                            type: "page",
+                            path: "./no-code-platforms/squarespace/video.mdx",
+                        },
+                    ]
+                },
+                {
+                    type: "page",
+                    path: "./no-code-platforms/shopify.mdx",
+                    pages: [
+                        {
+                            type: "page",
+                            path: "./no-code-platforms/shopify/setup.mdx",
+                        },
+                        {
+                            type: "page",
+                            path: "./no-code-platforms/shopify/video.mdx",
+                        },
+                    ]
+                },
+                {
+                    type: "page",
+                    path: "./no-code-platforms/framer.mdx",
+                    pages: [
+                        {
+                            type: "page",
+                            path: "./no-code-platforms/framer/setup.mdx",
+                        },
+                        {
+                            type: "page",
+                            path: "./no-code-platforms/framer/video.mdx",
+                        },
+                    ]
+                },
+                {
+                    type: "page",
+                    path: "./no-code-platforms/google-tag-manager.mdx",
+                    pages: [
+                        {
+                            type: "page",
+                            path: "./no-code-platforms/google-tag-manager/setup.mdx",
+                        },
+                        {
+                            type: "page",
+                            path: "./no-code-platforms/google-tag-manager/video.mdx",
+                        },
+                    ]
+                },
+                {
+                    type: "page",
+                    path: "./no-code-platforms/typedream.mdx",
+                    pages: [
+                        {
+                            type: "page",
+                            path: "./no-code-platforms/typedream/setup.mdx",
+                        },
+                        {
+                            type: "page",
+                            path: "./no-code-platforms/typedream/video.mdx",
+                        },
+                    ]
+                },
+            ],
+        },
+        {
+            type: "section",
+            label: "Web Frameworks",
+            visibility: "PUBLIC",
+            pages: [
+                {
+                    type: "page",
+                    path: "./web-frameworks/react.mdx",
+                    pages: [
+                        {
+                            type: "page",
+                            path: "./web-frameworks/react/setup.mdx",
+                        },
+                        {
+                            type: "page",
+                            path: "./web-frameworks/react/video.mdx",
+                        },
+                    ]
+                },
+                {
+                    type: "page",
+                    path: "./web-frameworks/next.mdx",
+                    pages: [
+                        {
+                            type: "page",
+                            path: "./web-frameworks/next/setup.mdx",
+                        },
+                        {
+                            type: "page",
+                            path: "./web-frameworks/next/video.mdx",
+                        },
+                    ]
+                },
+                {
+                    type: "page",
+                    path: "./web-frameworks/vue.mdx",
+                    pages: [
+                        {
+                            type: "page",
+                            path: "./web-frameworks/vue/setup.mdx",
+                        },
+                        {
+                            type: "page",
+                            path: "./web-frameworks/vue/video.mdx",
+                        },
+                    ]
+                },
+                {
+                    type: "page",
+                    path: "./web-frameworks/angular.mdx",
+                    pages: [
+                        {
+                            type: "page",
+                            path: "./web-frameworks/angular/setup.mdx",
+                        },
+                        {
+                            type: "page",
+                            path: "./web-frameworks/angular/video.mdx",
+                        },
+                    ]
+                },
+                {
+                    type: "page",
+                    path: "./web-frameworks/svelte.mdx",
+                    pages: [
+                        {
+                            type: "page",
+                            path: "./web-frameworks/svelte/setup.mdx",
+                        },
+                        {
+                            type: "page",
+                            path: "./web-frameworks/svelte/video.mdx",
+                        },
+                    ]
+                },
+            ]
+        },
+        {
+            type: "section",
+            label: "Integrations",
+            visibility: "PUBLIC",
+            pages: [
+                {
+                    type: "page",
+                    path: "./integrations/how-to-integrate-with-monday.mdx",
+                },
+                {
+                    type: "page",
+                    path: "./integrations/how-to-integrate-with-slack.mdx",
+                },
+                {
+                    type: "page",
+                    path: "./integrations/how-to-integrate-with-asana.mdx",
+                },
+                {
+                    type: "page",
+                    path: "./integrations/how-to-integrate-with-clickup.mdx",
+                },
+                {
+                    type: "page",
+                    path: "./integrations/how-to-use-webhooks.mdx",
+                },
+            ]
+        },
+        {
+            type: "section",
+            label: "Dashboard",
+            visibility: "PUBLIC",
+            pages: [
+                {
+                    type: "page",
+                    path: "./dashboard/how-to-access-project-settings.mdx",
+                },
+                {
+                    type: "page",
+                    path: "./dashboard/how-to-invite-users-to-your-project.mdx",
+                },
+                {
+                    type: "page",
+                    path: "./dashboard/how-to-view-all-comments-on-a-project.mdx",
+                },
+                {
+                    type: "page",
+                    path: "./dashboard/how-to-upload-custom-branding.mdx",
+                },
+                {
+                    type: "page",
+                    path: "./dashboard/navigating-the-support-section.mdx",
+                },
+                {
+                    type: "page",
+                    path: "./dashboard/how-to-create-a-new-project.mdx",
+                },
+                {
+                    type: "page",
+                    path: "./dashboard/how-to-invite-admin-users.mdx",
+                },
+                {
+                    type: "page",
+                    path: "./dashboard/how-to-invite-commenters.mdx",
+                },
+                {
+                    type: "page",
+                    path: "./dashboard/how-to-remove-admin-users.mdx",
+                },
+                {
+                    type: "page",
+                    path: "./dashboard/how-to-remove-commenters.mdx",
+                },
+                {
+                    type: "page",
+                    path: "./dashboard/how-to-use-the-kanban-board.mdx",
+                },
+            ]
+        },
+        {
+            type: "section",
+            label: "Files",
+            visibility: "PUBLIC",
+            pages: [
+                {
+                    type: "page",
+                    path: "./files/how-to-upload-a-file.mdx",
+                },
+                {
+                    type: "page",
+                    path: "./files/how-to-comment-on-a-file.mdx",
+                },
+                {
+                    type: "page",
+                    path: "./files/how-to-invite-commenters-to-a-file.mdx",
+                },
+                {
+                    type: "page",
+                    path: "./files/how-to-invite-admin-users-to-a-file.mdx",
+                },
+            ]
+        },
+        {
+            type: "section",
+            label: "Product Features",
+            visibility: "PUBLIC",
+            pages: [
+                {
+                    type: "page",
+                    path: "./product-features/what-is-guest-mode.mdx",
+                },
+                {
+                    type: "page",
+                    path: "./product-features/what-are-private-comments.mdx",
+                },
+                {
+                    type: "page",
+                    path: "./product-features/how-secure-is-superflow.mdx",
+                },
+                {
+                    type: "page",
+                    path: "./product-features/which-parts-of-the-site-would-i-be-able-to-custom-brand.mdx",
+                },
+            ]
+        },
+        {
+            type: "section",
+            label: "Billing",
+            visibility: "PUBLIC",
+            pages: [
+                {
+                    type: "page",
+                    path: "./billing/what-is-an-active-project.mdx",
+                },
+                {
+                    type: "page",
+                    path: "./billing/what-is-a-seat.mdx",
+                },
+                {
+                    type: "page",
+                    path: "./billing/do-you-offer-a-free-plan.mdx",
+                },
+                {
+                    type: "page",
+                    path: "./billing/do-you-offer-a-discount-to-education-institutes-or-non-profits.mdx",
+                },
+            ]
+        },
+        {
+            type: "section",
+            label: "Security",
+            visibility: "PUBLIC",
+            pages: [
+                {
+                    type: "page",
+                    path: "./security/content-security-policy.mdx",
+                },
+                {
+                    type: "page",
+                    path: "./security/auth-tokens.mdx",
+                },
+            ]
+        }
+    ],
+};
